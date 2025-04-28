8dc79a2a5bc12f14c31786a6c64e1ede90621181
rosita11-11 <itaaar44@gmail.com>
2025-04-28 09:34:36 +0700

    update main.js

--- main.js
+++ main.js
@@ -110,4 +110,22 @@ $(document).on("click", ".btn-status", function () {
   } else {
     statusBaru = "Belum Selesai";
   }
-  
+    // Update tampilan tombol
+  $(this).attr("data-status", statusBaru);
+  $(this).text(statusBaru);
+  updateWarnaStatus($(this), statusBaru);
+
+  // Tambahkan kode AJAX jika ingin menyimpan perubahan status ke database
+  console.log(`Status tugas ID ${tugasId} diubah menjadi ${statusBaru}`);
+});
+
+// Fungsi untuk memperbarui warna tombol berdasarkan status
+function updateWarnaStatus(button, status) {
+  if (status === "Belum Selesai") {
+    button.css("background-color", "#dc3545").css("color", "white");
+  } else if (status === "Sedang Dikerjakan") {
+    button.css("background-color", "#ffc107").css("color", "black");
+  } else {
+    button.css("background-color", "#28a745").css("color", "white");
+  }
+}
